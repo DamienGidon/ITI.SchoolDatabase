242140fcd37544f2c75d710daaceacfc196b7151
Thibault Cam <cam@intechinfo.fr>
2019-06-18 22:28:32 +0200

    fix

--- ITI-SchoolDatabase/ITI.SchoolDatabase.Impl/SqlScript/ModificationScheme/TeacherMod.sql
+++ ITI-SchoolDatabase/ITI.SchoolDatabase.Impl/SqlScript/ModificationScheme/TeacherMod.sql
@@ -1,2 +1,1 @@
-﻿-- Créer les colonnes Firstname, Lastname dans la table Student et la remplir en prenant nom prénom de la colonne Name 
--- pour les profs nés après 1975
+﻿-- Créer les colonnes Firstname, Lastname dans la table Teacher et les remplir uniquement pour les profs nés après 1975
